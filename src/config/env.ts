308e7566687243eb812e7eef746c66954abc1b6b
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-14 21:54:48 -0300

    feat: implementa schema

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import z4 from 'zod/v4';
+
+const envSchema = z4.object({
+  DATABASE_URL: z4
+    .url()
+    .default(
+      'postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public'
+    ),
+});
+
+export type Env = z4.infer<typeof envSchema>;
+
+export const env: Env = envSchema.parse(process.env);
