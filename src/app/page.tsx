290621bdff9f3db6d079670a739c54623d0561d4
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-16 18:57:35 -0300

    feat: implementa button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="size-screen overflow-hidden">
+      <figure className="bg-background-tertiary px-5 py-3 rounded-br-xl flex items-center gap-2 w-fit">
+        <Image
+          src="/icon.svg"
+          alt=""
+          height={20}
+          width={20}
+          className="size-5 aspect-square"
+        />
+        <figcaption className="uppercase text-content-brand typo-label-large">
+          Petshop
+        </figcaption>
+      </figure>
+
+      <main className="container p-20 my-8">
+        <header className="flex items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <h2 className="typo-title text-content-primary">Sua agenda</h2>
+            <p className="typo-paragraph-medium text-content-secondary">
+              Aqui você pode ver todos os clientes e serviços agendados para
+              hoje.
+            </p>
+          </div>
+        </header>
+      </main>
+    </section>
   );
 }
