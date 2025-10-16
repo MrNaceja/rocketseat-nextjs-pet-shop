c6bb8ff8f85f57f1adc847c9d3bb7b2aaaf159d7
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-16 19:13:41 -0300

    style: ajustes formatacao codigo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,28 +6,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'pgdata/**',
-      'src/generated/prisma/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    rules: {
-      'arrow-body-style': 'off',
-      'prefer-arrow-callback': 'off',
-      indent: ['error', 4],
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'pgdata/**',
+            'src/generated/prisma/**',
+            'build/**',
+            'next-env.d.ts',
+        ],
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        rules: {
+            'arrow-body-style': 'off',
+            'prefer-arrow-callback': 'off',
+            indent: ['error', 4],
+        },
     },
-  },
 ];
 
 export default eslintConfig;
